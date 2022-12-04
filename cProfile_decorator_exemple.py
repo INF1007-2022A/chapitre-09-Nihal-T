419118efc45e157ff5352542f18b102c3b0538c9
Nihal-T <112827282+Nihal-T@users.noreply.github.com>
2022-12-04 17:18:38 -0500

    S

--- cProfile_decorator_exemple.py
+++ cProfile_decorator_exemple.py
@@ -24,7 +24,6 @@ def time_function(func):
 @time_function
 def some_function():
     some_list = [i for i in range(1000)]
-
     new_list = []
     for elem in some_list:
         new_list.append([i for i in range(elem)])
--- error_type.py
+++ error_type.py
@@ -1,24 +1,21 @@
-def syntax_error(some_list)
+def syntax_error(some_list):
 
     return sorted(some_list)
 
 
 def sem_error(x, y):
 
-    return x + y
+    return (str(x) + y)
 
 
 def logical_error(count):
     x = 0
-    while count > 0:
+    if count > x:
         x += 1
-        count = x
-
     return x
 
 
 if __name__ == '__main__':
     syntax_error([3,2,1])
-    
     sem_error(5, "test")
     logical_error(8)
